113fe1aea5c62a482b057d9547c581a1012d0277
Valentin Thirion <valentinthirion@gmail.com>
2014-12-29 16:05:04 +0100

    Necessary starting files

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Invoice sent state",
+    'version': '1.0',
+    'depends': ['account_accountant'],
+    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Invoicing',
+    'description': """
+    This module adds a new state "sent" for the invoice. 
+    """,
+    'data': [],
+    'demo': [],
+}
